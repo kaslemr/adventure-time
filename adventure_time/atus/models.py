ab367783f8c9e82ae3a81c16ccdcd0aeaac815d7
kaslemr <mattkasle@gmail.com>
2015-11-05 13:31:45 -0500

    Created Models

---
+++ adventure_time/atus/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+class Respondent(models.Model):
+    case_id = models.IntegerField()
+    weight = models.IntegerField()
+    age = models.IntegerField()
+    gender = models.CharField(max_length=20)
+    activity_totals = models.ManyToManyField(Activity)
+
+
+
+class Activity(models.Model):
+    code = models.IntegerField()
+    daily_minutes = models.IntegerField()
+    total_respondents = models.IntegerField()
+    activity_title = models.CharField(100)
+
