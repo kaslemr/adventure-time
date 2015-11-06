6c19bea4c5dc1f602bde0184297416796076b6fb
kaslemr <mattkasle@gmail.com>
2015-11-06 14:18:51 -0500

    Continued manipulating data tables for migration

--- adventure_time/atus/models.py
+++ adventure_time/atus/models.py
@@ -3,16 +3,35 @@ from django.db import models
 # Create your models here.
 
 class Respondent(models.Model):
-    case_id = models.IntegerField()
-    weight = models.IntegerField()
+    respondent = models.FloatField()
+    weight = models.FloatField()
     age = models.IntegerField()
-    gender = models.CharField(max_length=20)
-    activity_totals = models.ManyToManyField(Activity)
+    sex = models.IntegerField()
+    education = models.IntegerField()
+    race = models.IntegerField()
+    metro_status = models.IntegerField()
+    labor_status = models.IntegerField()
+    wkly_earnings = models.IntegerField()
+    multiple_job_status = models.IntegerField()
+    full_or_part_job_status = models.IntegerField()
+    school_enrollment = models.IntegerField()
+    school_level = models.IntegerField()
+    typical_work_hrs = models.IntegerField()
+    activity_code = models.ManyToManyField(Activity)
 
 
 class Activity(models.Model):
-    code = models.IntegerField()
+    respondent = models.ForeignKey(Respondent)
+    activity_code = models.IntegerField()
+
     daily_minutes = models.IntegerField()
     total_respondents = models.IntegerField()
     activity_title = models.CharField(100)
+
+
+class Household(models.Model):
+    respondent = models.ForeignKey(Respondent)
+    activity_code = models.IntegerField()
+    minutes = models.IntegerField()
+
 
