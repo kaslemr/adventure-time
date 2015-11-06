e6568892144dd2f59b35a5a11202855225164fe7
TravisKnop <travisknop@gmail.com>
2015-11-05 22:12:53 -0500

    moved the play file to correct folder

--- adventure_time/play.py
+++ adventure_time/play.py
@@ -1,4 +1,15 @@
-from rest_framework.fields import CharField
+import pandas as pd
+
+
+def get_summary_data_from_atus_data():
+
+
+
+"""
+Do we want to clean the data up for human consumption (like below)?
+or leave it in API format?
+
+
 
 
 def sex_fix(sex):
@@ -13,3 +24,4 @@ def youngest_child_fix(youngest_child):
     for i in youngest_child:
         if i < 0:
             i = "NaN"
+"""
