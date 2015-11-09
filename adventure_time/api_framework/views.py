1c06b88723a09613096554a00349e344ec044ce6
kaslemr <mattkasle@gmail.com>
2015-11-08 21:20:53 -0500

    added household api views, 80% working

--- adventure_time/api_framework/views.py
+++ adventure_time/api_framework/views.py
@@ -2,7 +2,7 @@ from django.core import serializers
 from django.shortcuts import render
 from django.http import HttpResponse, QueryDict
 from django.views.decorators.csrf import csrf_exempt
-from atus.models import Respondent, ActivityResponse, ActivityTitle
+from atus.models import Respondent, ActivityResponse, ActivityTitle, HouseholdMember
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.serializers import ModelSerializer
 
@@ -27,11 +27,24 @@ def api_activitytitle_list_view(request):
     qs = ActivityTitle.objects.all()
     return HttpResponse(serializers.serialize("json", qs), content_type="application/json")
 
+@csrf_exempt
+def api_householdmember_list_view(request):
+    qs = HouseholdMember.objects.all()
+    return HttpResponse(serializers.serialize("json", qs), content_type="application/json")
+
+
+@csrf_exempt
+def api_householdmember_detail_view(request, model_pk):
+    qs = HouseholdMember.objects.filter(id=model_pk)
+    return HttpResponse(serializers.serialize("json", qs), content_type="application/json")
+
+
 
 class RespondentSerializer(ModelSerializer):
 
     class Meta:
         model = Respondent
+        lookup_field = 'respondent'
 
 
 class RespondentListView(ListAPIView):
@@ -42,6 +55,7 @@ class RespondentListView(ListAPIView):
 class RespondentDetailView(RetrieveAPIView):
     queryset = Respondent.objects.filter()
     serializer_class = RespondentSerializer
+    lookup_field = 'respondent'
 
 
 class ActivityResponseSerializer(ModelSerializer):
@@ -54,6 +68,7 @@ class ActivityTitleSerializer(ModelSerializer):
 
     class Meta:
         model = ActivityTitle
+        lookup_field = 'code'
 
 
 class ActivityResponseListView(ListAPIView):
@@ -61,9 +76,11 @@ class ActivityResponseListView(ListAPIView):
     serializer_class = ActivityResponseSerializer
 
 
+
 class ActivityResponseDetailView(RetrieveAPIView):
     queryset = ActivityResponse.objects.all()
     serializer_class = ActivityResponseSerializer
+
 
 
 class ActivityTitleListView(ListAPIView):
@@ -71,7 +88,37 @@ class ActivityTitleListView(ListAPIView):
     serializer_class = ActivityTitleSerializer
 
 
+
 class ActivityTitleDetailView(RetrieveAPIView):
     queryset = ActivityTitle.objects.all()
     serializer_class = ActivityTitleSerializer
+    lookup_field = 'code'
+
+
+class HouseholdMemberSerializer(ModelSerializer):
+
+    class Meta:
+        model = HouseholdMember
+        lookup_field = 'respondent'
+
+
+
+class HouseholdMemberListView(ListAPIView):
+    serializer_class = HouseholdMemberSerializer
+    queryset = HouseholdMember.objects.all()
+
+
+
+class HouseholdMemberDetailView(RetrieveAPIView):
+    serializer_class = HouseholdMemberSerializer
+    queryset = HouseholdMember.objects.all()
+    lookup_field = 'respondent'
+
+    #def get_queryset(self):
+        #respondent = self.kwargs['respondent']
+        #return HouseholdMember.objects.filter(respondent=respondent)
+
+
+
+
 
